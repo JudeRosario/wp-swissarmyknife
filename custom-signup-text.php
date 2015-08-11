d8d93802fe48fa3fd2a0cb6dc25cba1b66818976
Jude Rosario <judesrosario89@gmail.com>
2015-08-11 21:40:34 +0530

    Add string replacement function

--- custom-signup-text.php
+++ custom-signup-text.php
@@ -7,3 +7,24 @@ Author: Jude Rosario
 Author URI: http://premium.wpmudev.org/
 Version: 1.1
 */
+
+function themename_wp_signup_text( $translated_text, $untranslated_text, $domain ) {
+    global $pagenow;
+
+    if ( is_multisite() && $pagenow === 'wp-signup.php' ) {
+
+    switch ( $untranslated_text ) {
+            case 'Welcome back, %s. By filling out the form below, you can <strong>add another site to your account</strong>. There is no limit to the number of sites you can have, so create to your heart&#8217;s content, but write responsibly!' :
+            $translated_text = __( 'Welcome back, %s. By filling out the form below, you can add another site to your account.' , 'theme_text_domain' );
+            break;
+
+            case 'If you&#8217;re not going to use a great site domain, leave it for a new user. Now have at it!' :
+            $translated_text = __( ' ' , 'theme_text_domain' );
+            break;
+        }
+    }
+
+    return $translated_text;
+}
+
+add_filter( 'gettext', 'themename_wp_signup_text', 20, 3 );
