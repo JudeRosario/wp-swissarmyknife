7e317b4c7aa3a9a40c7df6c726de749eb3d4da0e
Jude Rosario <judesrosario89@gmail.com>
2015-08-11 21:39:30 +0530

    Add mu-plugin custom-sign up text

---
+++ custom-signup-text.php
@@ -0,0 +1,9 @@
+<?
+/*
+Plugin Name: Custom Signup text
+Plugin URI: https://premium.wpmudev.org/project/anti-splog/
+Description: MU Plugin that adds sanity to the wp-signup.php. 
+Author: Jude Rosario
+Author URI: http://premium.wpmudev.org/
+Version: 1.1
+*/
