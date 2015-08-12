582351664875b5250133a9468c0c4a6e25678191
Jude Rosario <judesrosario89@gmail.com>
2015-08-12 17:24:12 +0530

    Fix Plugin meta fixes

--- custom-signup-text.php
+++ custom-signup-text.php
@@ -1,7 +1,6 @@
-<?
+<?php
 /*
 Plugin Name: Custom Signup text
-Plugin URI: https://premium.wpmudev.org/project/anti-splog/
 Description: MU Plugin that adds sanity to the wp-signup.php. 
 Author: Jude Rosario
 Author URI: http://premium.wpmudev.org/
