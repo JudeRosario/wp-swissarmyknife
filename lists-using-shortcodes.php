9a591a0b06dc4a0ba1efa147358f8073239b326f
Jude Rosario <judesrosario89@gmail.com>
2015-08-13 05:44:48 +0530

    Add plugin to List stuff

---
+++ lists-using-shortcodes.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Lists in a Shortcode
+Description: MU Plugin that lets you get lists of posts, pages and sites in a network
+Author: Jude Rosario
+Author URI: http://premium.wpmudev.org/
+Version: 1.1
+*/
+
+
+
+
+?>
